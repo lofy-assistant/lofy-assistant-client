b3e3c6a785c4c0494253538ec8baa0695dba667d
ILHAM GHAZI BIN ISKANDAR AFIAN <ilhamghaz@gmail.com>
2026-01-03 16:06:18 +0800

    Changed next.config.ts for Cloud Tasks

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['@google-cloud/tasks'],
+  serverExternalPackages: ["@google-cloud/tasks"],
+  outputFileTracingIncludes: {
+    "/api/**": ["node_modules/@google-cloud/tasks/build/esm/src/v2/**/*.json", "node_modules/@google-cloud/tasks/build/src/v2/**/*.json"],
+  },
 };
 
 export default nextConfig;
