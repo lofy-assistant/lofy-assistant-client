cd45d9b3248dc7924cf1f29067d7b42969ca2805
Ecc3ntr1cSage <ecc3ntr1c.sage@gmail.com>
2026-01-25 18:45:00 +0800

    update striple plans for prod

--- lib/stripe-plans.ts
+++ lib/stripe-plans.ts
@@ -3,11 +3,11 @@ export const plans = [
     link:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_4gM00i40H5qWarl9fD2VG00"
-        : '',
+        : "https://buy.stripe.com/8x2cN7gq40TaeXjd2kdEs01",
     priceId:
       process.env.NODE_ENV === "development"
         ? "price_1StNzuBSILEw5PUUDdMQudxW"
-        : '',
+        : "price_1StQkIPXWy5Z5igt5cQiCxdR",
     price: 5,
     duration: "/month",
     billingCycle: "monthly" as const,
@@ -16,11 +16,11 @@ export const plans = [
     link:
       process.env.NODE_ENV === "development"
         ? "https://buy.stripe.com/test_fZu14mdBh1aGdDxezX2VG01"
-        : '',
+        : "https://buy.stripe.com/6oUeVfc9OgS8aH3bYgdEs02",
     priceId:
       process.env.NODE_ENV === "development"
-        ? 'price_1StO0RBSILEw5PUU1o2Xt1BT'
-        : '',
+        ? "price_1StO0RBSILEw5PUU1o2Xt1BT"
+        : "price_1StQkGPXWy5Z5igtaFZKxUyI",
     price: 45,
     duration: "/year",
     billingCycle: "yearly" as const,
