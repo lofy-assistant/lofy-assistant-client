a6f8d41fea84a42bad5971ffa8880eb409876d7b
Ecc3ntr1cSage <ecc3ntr1c.sage@gmail.com>
2026-01-31 12:01:43 +0800

    minor ui fix

--- components/brochure/home/cta-section.tsx
+++ components/brochure/home/cta-section.tsx
@@ -11,7 +11,7 @@ export default function CTASection() {
   return (
     <section className="relative overflow-hidden bg-background text-center pb-24">
       <div className="max-w-7xl mx-auto">
-        <div className="relative bg-foreground h-[50vh] flex items-center rounded-3xl mx-4 md:mx-8">
+        <div className="relative bg-foreground h-[50vh] flex items-center rounded-3xl md:mx-8">
           <div className="max-w-3xl mx-auto space-y-8">
             <h2 className="text-4xl font-bold leading-tight text-white">
                 It's free to get started.
